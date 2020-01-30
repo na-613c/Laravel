3a8adb1162a6fd57d5b2b6e520f31ae2c17ed4b4
Shavlovsii_Ivan <na613c613c@gmail.com>
2020-01-30 21:57:24 +0300

    [CREATE]

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\MainText;
+
+class PageController extends Controller
+{
+    public function getIndex($url = null)
+    {
+        if (!$url) {
+            $file = 'index';
+        } else {
+            $file = $url;
+        }
+        $obj = Maintext::where('url', $file)->first();
+        return view('page', compact('obj'));
+    }
+}
